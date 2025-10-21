acf7d6824209affe16f53997c6820371f5b54619
silgenius <olutademartin@gmail.com>
2025-10-21 14:53:39 +0100

    feat: Integrate fetching messages from server

--- components/DisplayUserMessages.js
+++ components/DisplayUserMessages.js
@@ -1,17 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { FlatList } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import AnimatedMessage from './AnimatedMessage';
 import useSocketListeners from '../hooks/useSocketListener';
 import messageEvents from '../socket/events/messageEvents';
+import { fetchMessagesByconversationId } from '../store/messagesSlice';
+import { selectMessagesForConversations } from '../store/selectors';
 
 export default function DisplayUserMessages({ conversationId }) {
   const flatListRef = useRef(null);
-  const dispatch = useDispatch()
-  const messages = useSelector((state) => state.messages[conversationId]);
+  const dispatch = useDispatch();
+  const messages = useSelector((state) =>
+    selectMessagesForConversations(state, conversationId)
+  );
   const userId = useSelector((state) => state.user.id);
+
   // useSocketListeners(messageEvents(dispatch));
+
+  useEffect(() => {
+    dispatch(fetchMessagesByconversationId(conversationId)); // fetch messages only if messsages is null
+  }, [dispatch, conversationId]);
 
   const scrollToItem = (id) => {
     const index = messages.findIndex(
