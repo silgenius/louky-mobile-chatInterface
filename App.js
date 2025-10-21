84973945bee171895332f09fbc9aef09f1eacc34
silgenius <olutademartin@gmail.com>
2025-10-21 15:07:31 +0100

    feat: Update App theme

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { PaperProvider } from 'react-native-paper';
+import {
+  PaperProvider,
+  MD3LightTheme as DefaultTheme,
+} from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
 
 import store from './store/store';
 import UserMessagesScreen from './screens/UserMessagesScreeen';
-import UserConversationScreen from './screens/UserConversationScreen'
+import UserConversationScreen from './screens/UserConversationScreen';
+import COLORS from './constants/colors';
+
+const { colors } = COLORS.appLightColor;
+const theme = {
+  ...DefaultTheme,
+  colors,
+};
 
 export default function App() {
   return (
     <StoreProvider store={store}>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <SafeAreaProvider>
           <UserMessagesScreen />
         </SafeAreaProvider>
